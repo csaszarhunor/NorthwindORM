86d9b07b2686f076562df76601b674d2fa587537
csaszarhunor <csaszarhunor@gmail.com>
2016-01-20 15:09:44 +0100

    customer export to csv

--- main.py
+++ main.py
@@ -10,6 +10,21 @@ def get_csv_rows(file_loc):
     return rows[1:len(rows)]
 
 
+def get_db_records(cursor_obj, table):
+    cursor_obj.execute("SELECT * FROM {}".format(table))
+    records = cursor_obj.fetchall()
+    csv_rows = []
+    for rec in records:
+        row = ""
+        for column in rec:
+            if column == None:
+                row += ";"
+            else:
+                row += column + ";"
+        csv_rows.append(row[:-1])
+    return csv_rows
+
+
 def import_from_csv(csv, clss, db_table):
     cs_rows = get_csv_rows(csv)
     cnx = mysql.connector.connect(user='root', password="F0i1b1o2n3a5c8c13i21", database='northwind')
@@ -26,4 +41,14 @@ def import_from_csv(csv, clss, db_table):
     cursor.close()
     cnx.close()
 
-import_from_csv("csv/customers.csv", Customer, "customersclone")
+
+def export_to_csv(db_table, clss, csv_loc):
+    cnx = mysql.connector.connect(user='root', password="F0i1b1o2n3a5c8c13i21", database='northwind')
+    cursor = cnx.cursor()
+    csv_rows = get_db_records(cursor, db_table)
+    with open(csv_loc, 'w', encoding="utf8") as csv_file:
+        for row in csv_rows:
+            csv_file.write(row + "\n")
+    cursor.close()
+    cnx.close()
+
